1a6202306e7d049c32bce47185c84ce2ba95e7b2
mrdofu <intense_smurf@live.com>
2016-01-24 18:21:26 -0500

    added files

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+	bool facingRight = true;
+	public float maxSpeed = 10f;
+
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	// Use this for initialization
+	void Start () {
+		// we'll be changing its parameters
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		// left and right movement
+		anim.SetFloat ("Speed", Mathf.Abs (move * maxSpeed));
+		rigidbody2D.velocity = new Vector2 (move*maxSpeed, rigidbody2D.velocity.y);
+
+		// jumping check for grounded
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+		anim.SetFloat ("vSpeed", Mathf.Abs (rigidbody2D.velocity.y));
+
+		// left or right facing
+		if (move > 0 && !facingRight) 
+			flip ();
+		else if (move < 0 && facingRight)
+			flip ();
+	}
+
+	void Update(){
+		// jumping
+		if (grounded && Input.GetAxis ("Vertical")>0) {
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
